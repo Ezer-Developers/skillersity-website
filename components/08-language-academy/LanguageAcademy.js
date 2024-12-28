d5d3936fbe9477db83080912d8ee68af02eea935
Ezer Developers <ezerdevelopers@gmail.com>
2024-12-28 17:56:31 +0530

    Popup form on home screen.

--- components/08-language-academy/LanguageAcademy.js
+++ components/08-language-academy/LanguageAcademy.js
@@ -2,36 +2,36 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from "react";
 import sal from "sal.js";
-
+import React, { useEffect, useState } from "react";
 import CourseDetails from "../../data/course-details/courseData.json";
 import Card from "../Cards/Card";
 import TestimonialSeven from "../Testimonials/Testimonial-Seven"
-import BrandOne from "../Brand/Brand-One";
 import TeamThree from "../Team/TeamThree"
 import SplitTwo from "../Split/Split-Two";
 import Newsletter from "../Newsletters/Newsletter";
-
-import client1 from "../../public/images/testimonial/client-04.png";
-import client2 from "../../public/images/testimonial/client-05.png";
-import client3 from "../../public/images/testimonial/client-06.png";
-import client4 from "../../public/images/testimonial/client-12.png";
 import languageClub from "../../public/images/banner/language-club.png";
-
-import CategoryFour from "../Category/CategoryFour";
 import AboutTwo from "../Abouts/About-Two";
 import CountDownTwo from "../Counters/CountDownTwo";
 import { ParallaxProvider } from "react-scroll-parallax";
+import PopupForm from "../Forms/PopupForm";
 
 const LanguageAcademy = ({ blogs }) => {
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  const togglePopup = () => {
+    setPopupOpen((prev) => !prev);
+  };
+
   let getAllCourse = JSON.parse(JSON.stringify(CourseDetails.languageAcademy));
+
   useEffect(() => {
     sal({
       threshold: 0.01,
       once: true,
     });
   }, []);
+
   return (
     <>
       <div className="rbt-banner-area rbt-banner-7 bg-gradient-1 theme-shape header-transperent-spacer">
@@ -49,64 +49,13 @@ const LanguageAcademy = ({ blogs }) => {
                     <h1 className="title">
                       Advancing
                       <span className="theme-gradient"> Your Career with</span> Practical Learning</h1>
-                    {/* <div className="rbt-like-total">
-                      <div className="profile-share">
-                        <Link
-                          href="#"
-                          className="avatar"
-                          data-tooltip="Mark JOrdan"
-                          tabIndex="0"
-                        >
-                          <Image
-                            src={client1}
-                            width={55}
-                            height={55}
-                            alt="education"
-                          />
-                        </Link>
-                        <Link
-                          href="#"
-                          className="avatar"
-                          data-tooltip="Mark"
-                          tabIndex="0"
-                        >
-                          <Image
-                            src={client2}
-                            width={55}
-                            height={55}
-                            alt="education"
-                          />
-                        </Link>
-                        <Link
-                          href="#"
-                          className="avatar"
-                          data-tooltip="Jordan"
-                          tabIndex="0"
-                        >
-                          <Image
-                            src={client3}
-                            width={55}
-                            height={55}
-                            alt="education"
-                          />
-                        </Link>
-                        <div className="more-author-text">
-                          <h5 className="total-join-students">
-                            Join Over 3000+ Students
-                          </h5>
-                          <p className="subtitle">
-                            Have a new ideas every week.
-                          </p>
-                        </div>
-                      </div>
-                    </div> */}
                     <div className="rbt-button-group justify-content-start mt--30">
-                      <Link
+                      <button
                         className="rbt-btn btn-gradient rbt-switch-btn"
-                        href="#"
+                        onClick={togglePopup}
                       >
-                        <span data-text="Sign Up Now">Enquire Now</span>
-                      </Link>
+                        <span data-text="Enquire Now">Enquire Now</span>
+                      </button>
                       <Link
                         className="rbt-btn btn-border rbt-switch-btn"
                         href="#"
@@ -127,31 +76,6 @@ const LanguageAcademy = ({ blogs }) => {
                       alt="Education Images"
                     />
                   </div>
-                  {/* <div className="card-info bounce-slide">
-                    <div className="inner">
-                      <div className="name">
-                        Hillery. <span>/ USA</span>
-                      </div>
-                      <div className="rating-wrapper d-block d-sm-flex">
-                        <div className="rating">
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                        </div>
-                        <span>(Google Review)</span>
-                      </div>
-                    </div>
-                    <div className="notify-icon">
-                      <Image
-                        src={client4}
-                        width={100}
-                        height={100}
-                        alt="Client Images"
-                      />
-                    </div>
-                  </div> */}
                 </div>
               </div>
             </div>
@@ -159,104 +83,7 @@ const LanguageAcademy = ({ blogs }) => {
         </div>
       </div>
 
-      {/* <div className="rbt-categories-area bg-color-extra2 rbt-section-gap">
-        <div className="container">
-          <div className="row g-5">
-            <CategoryFour />
-          </div>
-        </div>
-      </div> */}
-
-      {/* <div className="rbt-course-card-area rbt-section-gap bg-color-white">
-        <div className="container">
-          <div className="row align-items-center mb--60">
-            <div className="col-lg-12">
-              <div className="section-title text-center">
-                <span className="subtitle bg-pink-opacity">
-                  Our Language Courses
-                </span>
-                <h2 className="title">Our Professional Courses</h2>
-                <p className="description has-medium-font-size mt--20">
-                  Scale up your career supereasy with advanced tools.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="row g-5">
-            {CourseDetails &&
-              getAllCourse.map((data, index) => (
-                <div
-                  className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12"
-                  data-sal-delay="150"
-                  data-sal="slide-up"
-                  data-sal-duration="800"
-                  key={index}
-                >
-                  <div className={`rbt-card variation-03 rbt-hover`}>
-                    <div className="rbt-card-img">
-                      <Link
-                        className="thumbnail-link"
-                        href={`/course-details/${data.id}`}
-                      >
-                        <Image
-                          src={data.courseImg}
-                          width={355}
-                          height={244}
-                          alt="Card image"
-                        />
-                        <span className="rbt-btn btn-white icon-hover btn-md">
-                          <span className="btn-text">Read More</span>
-                          <span className="btn-icon">
-                            <i className="feather-arrow-right"></i>
-                          </span>
-                        </span>
-                      </Link>
-                    </div>
-                    <div className="rbt-card-body">
-                      <h4 className="rbt-card-title">
-                        <Link href={`/course-details/${data.id}`}>
-                          {data.courseTitle}
-                        </Link>
-                      </h4>
-
-                      <div className="rbt-card-bottom">
-                        <Link
-                          className="transparent-button"
-                          href={`/course-details/${data.id}`}
-                        >
-                          <svg
-                            width="17"
-                            height="12"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <g stroke="#27374D" fill="none" fillRule="evenodd">
-                              <path d="M10.614 0l5.629 5.629-5.63 5.629"></path>
-                              <path
-                                strokeLinecap="square"
-                                d="M.663 5.572h14.594"
-                              ></path>
-                            </g>
-                          </svg>
-                        </Link>
-                      </div>
-                    </div>
-                    <div className="card-information">
-                      <div className="card-flag">
-                        <Image
-                          src={data.countryFlag}
-                          width={21}
-                          height={21}
-                          alt="united-kingdom"
-                        />
-                      </div>
-                      <div className="card-count">{data.lesson} programs</div>
-                    </div>
-                  </div>
-                </div>
-              ))}
-          </div>
-        </div>
-      </div> */}
+      <PopupForm isOpen={isPopupOpen} onClose={togglePopup} />
 
       <div className="rbt-about-area bg-color-white csm-rbt-section-gapBtm pb_md--80 pb_sm--80 about-style-1">
         <div className="container">
@@ -313,12 +140,6 @@ const LanguageAcademy = ({ blogs }) => {
           </div>
         </div>
 
-        {/* <div className="rbt-counterup-area bg-color-extra2 rbt-section-gapBottom">
-          <div className="container">
-            <Counter isDesc={false} />
-          </div>
-        </div> */}
-
         <div className="rbt-about-area bg-color-white pb_md--80 pb_sm--80 about-style-1" style={{ paddingTop: "40px" }}>
           <div className="container">
             <SplitTwo />
@@ -345,25 +166,6 @@ const LanguageAcademy = ({ blogs }) => {
           </div>
           <TestimonialSeven />
         </div>
-
-        {/* <div className="rbt-brand-area bg-color-white rbt-section-gap">
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-lg-12">
-                <div className="section-title text-center mb--40">
-                  <span className="theme-gradient d-block small-title w-600 mb--10">
-                    Trusted by over 100 great teams
-                  </span>
-                  <p>
-                    Leading companies use the same courses to help employees keep
-                    their skills fresh.
-                  </p>
-                </div>
-                <BrandOne />
-              </div>
-            </div>
-          </div>
-        </div> */}
 
         <div className="rbt-team-area bg-color-white rbt-section-gap">
           <div className="container">
---
+++ components/Forms/PopupForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const PopupForm = ({ isOpen, onClose }) => {
+    if (!isOpen) return null; // Don't render the modal if not open
+
+    return (
+        <div
+            className={`popup-overlay ${isOpen ? "open" : ""}`} // Apply 'open' class to show modal
+            onClick={onClose}
+        >
+            <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+                <button className="close-button" onClick={onClose}>
+                    &times;
+                </button>
+                <h2>Enquire Now</h2>
+                <form>
+                    <div className="form-group">
+                        <input type="text" id="name" name="name" required placeholder="Name" />
+                    </div>
+                    <div className="form-group">
+                        <input type="tel" id="phone" name="phone" required placeholder="Phone" />
+                    </div>
+                    <div className="form-group">
+                        <input type="email" id="email" name="email" required placeholder="Email" />
+                    </div>
+                    <div className="form-group">
+                        <textarea id="message" name="message" rows="4" required placeholder="Message"></textarea>
+                    </div>
+                    <button type="submit" className="rbt-btn btn-gradient">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default PopupForm;
